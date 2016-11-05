00a2515cbb00b69cf51c55db23ba2d856d91cf12
Nicolas Friedli <nicolas@theologique.ch>
2016-11-06 00:05:38 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['btn_submit_answer']     = 'Soumettre sa réponse';
+$lang['btn_previous_submissions'] = 'Soumissions précédentes';
+$lang['btn_create_question_page'] = 'Créer une page de question';
+$lang['btn_submit_correct_answer'] = 'Soumettre la réponse correcte';
+$lang['btn_submit_testcase']   = 'Soumettre des tests';
+$lang['btn_submit_testcase_short'] = 'Court';
+$lang['btn_submit_testcase_long'] = 'Long';
+$lang['btn_edit_testcase_short'] = 'Éditer les tests courts';
+$lang['btn_edit_testcase_long'] = 'Éditer les tests longs';
+$lang['btn_edit_correct_answer'] = 'Éditer la réponse correcte';
+$lang['choose_language']       = 'Langue:';
+$lang['btn_choose_file']       = 'Choisir un fichier';
+$lang['btn_submit']            = 'Soumettre';
+$lang['err_file_format']       = 'Le format de fichier n\'est pas accepté.';
+$lang['err_file_exist']        = 'Le fichier existe sur le serveur.';
+$lang['err_upload_dir']        = 'n\'existe pas sur le serveur. Il faut le créer ou préciser un autre répertoire dans la configuration du plugin.';
+$lang['err_dir']               = 'répertoire';
+$lang['count_number']          = '#';
+$lang['timestamp']             = 'Heure';
+$lang['status']                = 'Statut';
+$lang['sender']                = 'Expéditeur';
+$lang['language']              = 'Langue';
+$lang['table_update']          = 'Mise à jour';
+$lang['runtime_error']         = 'Erreur d\'exécution';
+$lang['compile_error']         = 'Erreur de compilation';
+$lang['wrong']                 = 'Faux';
+$lang['correct']               = 'Correct';
+$lang['btn_my_submissions']    = 'Mes soumissions';
+$lang['programming_questions'] = 'Questions programmées';
+$lang['outputonly_questions']  = 'Question de sortie seulement';
+$lang['programming_question']  = 'Programmation';
+$lang['outputonly_question']   = 'Sortie seulement';
+$lang['question_name']         = 'Intitulé de la question';
+$lang['btn_export_csv']        = 'Exporter en CSV';
+$lang['menu']                  = 'Voir & supprimer les soumissions des utilisateurs';
+$lang['btn_get_submissions']   = 'Voir';
+$lang['btn_delete_submissions'] = 'Supprimer';
+$lang['all_users']             = 'Tous les utilisateurs';
+$lang['intro_message']         = 'Remplir le formulaire avec les paramètres des utilisateurs, puis appuyer sur le bouton «voir» ou le bouton «effacer»';
+$lang['empty_result']          = 'Pas de résultat.';
+$lang['delete_success']        = 'Soumissions effacées avec succès';
+$lang['js']['error']           = 'Erreur:';
+$lang['js']['answer_not_specified'] = 'Réponse non précisée.';
+$lang['js']['file_not_specified'] = 'Fichier non précisé.';
+$lang['js']['answer_status']   = 'Statut de la réponse:';
+$lang['js']['correct']         = 'correct';
+$lang['js']['wrong']           = 'faux';
---
+++ lang/fr/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['upload_path']           = 'chemin du répertoire comprenent les envois de code';
+$lang['editors_group']         = 'groupes d\'utilisateurs ayant droit aux tests et aux pages de réponses';
