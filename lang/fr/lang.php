a58832e1897f36a22e57e570a8cee6d2ceaa8bae
ubibene <services.m@benard.info>
2017-05-25 07:20:05 +0200

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author ubibene <services.m@benard.info>
+ */
+$lang['btn_submit_answer']     = 'Soumettre la Réponse';
+$lang['btn_previous_submissions'] = 'Soumission Précédente';
+$lang['btn_create_question_page'] = 'Créer la Page de Question';
+$lang['btn_submit_correct_answer'] = 'Soumettre La Réponse Correcte';
+$lang['btn_submit_testcase']   = 'Soumettre les Cas de Tests';
+$lang['btn_submit_testcase_short'] = 'Court';
+$lang['btn_submit_testcase_long'] = 'Long';
+$lang['btn_edit_testcase_short'] = 'Modifier les Cas de Tests Courts';
+$lang['btn_edit_testcase_long'] = 'Modifier les Cas de Tests Longs';
+$lang['btn_edit_correct_answer'] = 'Modifier la Réponse Correcte';
+$lang['choose_language']       = 'Langue:';
+$lang['btn_choose_file']       = 'Choisir un Fichier';
+$lang['btn_submit']            = 'Soumettre';
+$lang['err_file_format']       = 'Format de fichier non autorisé.';
+$lang['err_file_exist']        = 'Le fichier existe sur le serveur.';
+$lang['err_upload_dir']        = 'n\'existe pas sur le serveur. créer le ou définir un autre répertoire dans les configurations de plugin.';
+$lang['err_dir']               = 'Répertoire';
+$lang['count_number']          = 'N°';
+$lang['timestamp']             = 'Heure';
+$lang['status']                = 'Statut';
+$lang['sender']                = 'Expéditeur';
+$lang['language']              = 'Langue';
+$lang['table_update']          = 'Mettre à jour';
+$lang['runtime_error']         = 'Erreur d\'Exécution';
+$lang['compile_error']         = 'Erreur de Compilation';
+$lang['wrong']                 = 'Faux';
+$lang['correct']               = 'Correct';
+$lang['btn_my_submissions']    = 'Mes soumissions';
+$lang['outputonly_questions']  = 'Afficher seulement les questions.';
+$lang['programming_question']  = 'Programmation';
+$lang['question_name']         = 'Nom de la Question';
+$lang['btn_export_csv']        = 'Exporter en CSV';
+$lang['menu']                  = 'Voir & Supprimer les Soumissions';
+$lang['btn_get_submissions']   = 'Voir';
+$lang['btn_delete_submissions'] = 'Supprimer';
+$lang['all_users']             = 'Tous les utilisateurs';
+$lang['intro_message']         = 'Remplir le formulaire avec les paramètre de soumission des utilisateurs, puis appuyer sur le bouton voir ou supprimer.';
+$lang['empty_result']          = 'Pas de résultat.';
+$lang['delete_success']        = 'Soumissions supprimées avec succès.';
+$lang['js']['error']           = 'Erreur:';
+$lang['js']['answer_not_specified'] = 'Réponse non spécifiée.';
+$lang['js']['file_not_specified'] = 'Fichier non spécifié.';
+$lang['js']['answer_status']   = 'Statut de la réponse:';
+$lang['js']['correct']         = 'correct';
+$lang['js']['wrong']           = 'faux';
---
+++ lang/fr/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author ubibene <services.m@benard.info>
+ */
+$lang['upload_path']           = 'chemin vers le répertoire où téléverser les codes de soumission';
+$lang['editors_group']         = 'groupe d\'utilisateurs ayant accès aux cas de tests et pages de réponses';
