16224b2e714fd1fd9ec2c61a245323eb4fef8b09
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 13:30:39 +0200

    translation update

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['btn_submit_answer']     = 'Envie a resposta';
+$lang['btn_previous_submissions'] = 'Submissões anteriores';
+$lang['btn_create_question_page'] = 'Criar página de pergunta';
+$lang['btn_submit_correct_answer'] = 'Envie a resposta correta';
+$lang['btn_submit_testcase']   = 'Enviar casos de teste';
+$lang['btn_submit_testcase_short'] = 'Curto';
+$lang['btn_submit_testcase_long'] = 'Longo';
+$lang['btn_edit_testcase_short'] = 'Editar casos de teste curtos';
+$lang['btn_edit_testcase_long'] = 'Editar casos de teste longos';
+$lang['btn_edit_correct_answer'] = 'Editar resposta correta';
+$lang['choose_language']       = 'Linguagem:';
+$lang['btn_choose_file']       = 'Escolha o arquivo';
+$lang['btn_submit']            = 'Enviar';
+$lang['err_file_format']       = 'Formato do arquivo não permitido.';
+$lang['err_file_exist']        = 'O Arquivo existe no servidor.';
+$lang['err_upload_dir']        = 'não existe no servidor. crie-o ou especifique outro diretório nas configurações do plugin.';
+$lang['err_dir']               = 'diretório';
+$lang['count_number']          = '#';
+$lang['timestamp']             = 'Tempo';
+$lang['status']                = 'Status';
+$lang['sender']                = 'Remetente';
+$lang['language']              = 'Linguagem';
+$lang['table_update']          = 'Atualizar';
+$lang['runtime_error']         = 'Erro de tempo de execução';
+$lang['compile_error']         = 'Erro de compilação';
+$lang['wrong']                 = 'Errado';
+$lang['correct']               = 'Correto';
+$lang['btn_my_submissions']    = 'Meus envios';
+$lang['programming_questions'] = 'Perguntas de programação';
+$lang['outputonly_questions']  = 'Apenas perguntas de saída';
+$lang['programming_question']  = 'Programação';
+$lang['outputonly_question']   = 'Apenas saída';
+$lang['question_name']         = 'Nome da pergunta';
+$lang['btn_export_csv']        = 'Exportar para CSV';
+$lang['menu']                  = 'Visualizar e excluir envios de usuários';
+$lang['btn_get_submissions']   = 'Visualizar';
+$lang['btn_delete_submissions'] = 'Delete';
+$lang['all_users']             = 'Todos usuários';
+$lang['intro_message']         = 'Preencha o formulário a seguir com os parâmetros de envio de usuários e, em seguida, pressione o botão de visualização ou exclusão.';
+$lang['empty_result']          = 'Sem resultado.';
+$lang['delete_success']        = 'Envios deletado com sucesso.';
+$lang['js']['error']           = 'Error:';
+$lang['js']['answer_not_specified'] = 'Resposta não especificada.';
+$lang['js']['file_not_specified'] = 'Arquivo não especificado.';
+$lang['js']['answer_status']   = 'Status da resposta:';
+$lang['js']['correct']         = 'correto';
+$lang['js']['wrong']           = 'errado';
---
+++ lang/pt-br/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['upload_path']           = 'caminho para o diretório cujos códigos de envio foram enviados para';
+$lang['editors_group']         = 'grupo de usuários com acesso as páginas de casos de testes e respostas';
