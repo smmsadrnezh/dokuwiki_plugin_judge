b236de032c6ed7a6fdc92592cf5a879d50cd5b01
Minh <phandinhminh@protonmail.ch>
2023-01-24 23:05:05 +0100

    translation update

---
+++ lang/vi/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Minh <phandinhminh@protonmail.ch>
+ */
+$lang['upload_path']           = 'đường dẫn tới thư mục mà các mã code đệ trình tải lên tới';
+$lang['editors_group']         = 'nhóm người dùng với khả năng truy cập vào testcase và các trang đáp án';
